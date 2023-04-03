a2fa7ac2a1f0336d7eaae76cada963ecb9ec8e51
impath96 <impath96@gmail.com>
2023-04-03 10:44:24 +0900

    feat : [003-EMAIL-VERIFICATION] 고객 이메일 verification

--- user-api/src/main/java/com/zerobase/cms/user/configuration/GlobalFeignConfiguration.java
+++ user-api/src/main/java/com/zerobase/cms/user/configuration/GlobalFeignConfiguration.java
@@ -6,4 +6,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableFeignClients(basePackages = "com.zerobase.cms.user")
 public class GlobalFeignConfiguration {
+
 }
